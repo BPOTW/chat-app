a2b147fbcf465f26d287d640e75bdc5ca2c2e779
BPOTW <133132057+BPOTW@users.noreply.github.com>
2025-11-09 23:38:22 +0500

    commit 2nd

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,116 @@
 import './App.css'
 import Search from "./assets/search.svg"
+import Send from "./assets/send.svg"
 import { useState, useRef, useEffect } from 'react';
+import { io } from "socket.io-client";
+
+const SOCKET_URL = "http://localhost:5050";
+let socket;
 
 function App() {
 
-  const [messages, setMessages] = useState([
-    { id: 1, text: 'Welcome to the chat', sender: 'sender-id' },
-    { id: 1, text: 'Welcome to the chat', sender: 'me' },
-    { id: 1, text: 'Welcome to the chat', sender: 'sender-id' },
-    { id: 1, text: 'Welcome to the chat', sender: 'sender-id' },
-    { id: 1, text: 'Welcome to the chat', sender: 'me' },
-  ]);
+  const [messages, setMessages] = useState([]);
+  const [myId, setmyId] = useState('');
+  const [roomId, setroomId] = useState('');
   const [input, setInput] = useState('');
+  const [searchInput, setsearchInput] = useState('');
+
   const messagesEndRef = useRef(null);
 
+
+  const [receivedRequest, setreceivedRequest] = useState({ id: "", received: false });
+  const [joined, setJoined] = useState(false);
+  const [checkIdResult, setcheckIdResult] = useState({ id: "", isAvailable: false });
+  const [isConnected, setisConnected] = useState(false);
+  const [connectedChat, setconnectedChat] = useState("");
+  const [participents, setparticipents] = useState([]);
+
+
   useEffect(() => {
+    socket = io(SOCKET_URL);
+    socket.on("connect", () => {
+      console.log("Connected to server")
+      setmyId(socket.id);
+      setisConnected(true);
+      handleRegister();
+    });
+
+    socket.on('checkIdResult', (data) => {
+      // console.log(data);
+      setcheckIdResult(data);
+    })
+
+    socket.on('checkRoomResult', (data) => {
+      console.log(data);
+      handleJoinChat(data.id);
+    })
+
+    socket.on('request', (id) => {
+      // console.log(id);
+      setreceivedRequest({ id: id, received: true });
+    })
+
+    socket.on("message", (msg) => {
+      console.log(msg)
+      setMessages((prev) => [...prev, msg]);
+    });
+
+    socket.on("disconnect", () => {
+      setisConnected(false);
+      setmyId('Not Connected');
+      console.log("Disconnected")
+    });
+
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
-  }, [messages]);
+    return () => socket.disconnect();
+  }, []);
+
+  function handleSearchInput(e) {
+    setsearchInput(e.target.value)
+    console.log(e.target.value);
+  }
+
+  function handleSearchId(e) {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      console.log('enter pressed')
+      e.preventDefault();
+      searchForId();
+    }
+  }
+
+  const searchForId = () => {
+    socket.emit("checkId", searchInput);
+  };
+
+  const handleRegister = () => {
+    socket.emit("register", myId);
+  };
+
+  const handleSendRequest = () => {
+    socket.emit("sendRequest", { senderId: myId, receiverId: checkIdResult.id });
+    handleJoinChat(myId)
+  };
+
+  function cancelRequest() {
+    setreceivedRequest({id:'',received:false});
+  }
+
+  async function acceptRequest(){
+    socket.emit("checkRoom", { roomId:receivedRequest.id, senderId:myId});
+  }
+
+  function handleJoinChat(id){
+      socket.emit("join", { chatId: id, userId: myId });
+      setJoined(true);
+      setconnectedChat(id);
+      setreceivedRequest({id:'',received:false});
+  }
 
   const sendMessage = () => {
     const text = input.trim();
     if (!text) return;
-    setMessages((prev) => [...prev, { id: Date.now(), text, sender: 'me' }]);
+    // setMessages((prev) => [...prev, {text:text, id:myId}]);
+    socket.emit("send", { chatId: connectedChat, msg:text, id: myId });
     setInput('');
   };
 
@@ -35,53 +124,93 @@ function App() {
   return (
     <>
       <div className='main-div'>
+        {
+          isConnected ?
+            <div className='connected'>Connected</div> :
+            <div className='disconnected'>Disconnected</div>
+        }
+
+        {
+          receivedRequest.received ? 
+          <div className='request-div'>
+          <div className='request-msg-div'>gGaglmUXuhxSsbVuAAAH Wants to chat with you.</div>
+          <div className='request-btns-div'>
+            <div className='request-cancel' onClick={cancelRequest}>Cancel</div>
+            <div className='request-accept' onClick={acceptRequest}>Accept</div>
+          </div>
+        </div>
+        :
+        <div></div>
+        }
         <div className='chat-settings-div'>
 
           <div className='settings-div'>
 
-            <h2 className="settings-title">Settings</h2>
-
-            <div className="search-bar" role="search">
-              <div className="search-icon"><img src={Search} alt="" /></div>
-              <input
-                className="search-input"
-                type="search"
-                placeholder="Search friends..."
-              />
-            </div>
-
             <p className="title">My ID</p>
             <div className='myId-div textBox-div'>
-              AHS23YFH57DG
+              {myId}
             </div>
 
             <div className='spacer'></div>
 
-            <div className='participent-title-count-div'>
-              <p className="participents-title">Participents</p>
-              <p className='participents-count'>( 9 )</p>
-            </div>
-            <div className='participents-div'>
-              <div className='participent textBox-div'>
-                AHS23YFH57DG
-              </div>
-            </div>
+            {
+              !joined ?
+                <div className='search-div'>
+                  <h2 className="search-title">Search</h2>
 
+                  <div className="search-bar" role="search">
+                    <div className="search-icon"><img src={Search} alt="" /></div>
+                    <input
+                      className="search-input"
+                      type="search"
+                      placeholder="Search friends..."
+                      onChange={handleSearchInput}
+                      onKeyDown={handleSearchId}
+                    />
+                  </div>
+                  {
+                    checkIdResult.isAvailable ?
+                      <div className='searchResult textBox-div'>
+                        {checkIdResult.id}
+                        <img src={Send} width={20} className='sendRequest-icon' onClick={handleSendRequest} alt="" />
+                      </div>
+                      :
+                      <div></div>
+                  }
+                </div>
+                :
+                <div className='main-participents-div'>
+                  <h2 className="search-title">Joined Room ID</h2>
+                  <div className='joinedRoomId-div textBox-div'>
+                    {connectedChat}
+                  </div>
+                  <div className='spacer'></div>
+                  <div className='participent-title-count-div'>
+                    <p className="participents-title">Participents</p>
+                    <p className='participents-count'>( 9 )</p>
+                  </div>
+                  <div className='participents-div'>
+                    <div className='participent textBox-div'>
+                      AHS23YFH57DG
+                    </div>
+                  </div>
+                </div>}
 
+            {/* <div className='spacer'></div> */}
 
           </div>
 
           <div className='chat-div'>
             <div className='messages' role='log' aria-live='polite'>
-              {messages.map((m) => (
+              {messages.map((m,index) => (
                 <div
-                  key={m.id}
-                  className={`message ${m.sender === 'me' ? 'message-sent' : 'message-received'}`}>
+                  key={index}
+                  className={`message ${m.id == myId ? 'message-sent' : 'message-received'}`}>
                   <div className='message-info-div'>
                     {m.id} : 10:40pm
                   </div>
                   <div>
-                  {m.text}
+                    {m.text}
                   </div>
                 </div>
               ))}
@@ -103,7 +232,7 @@ function App() {
           </div>
         </div>
         <div className='footer-div'>
-          Made with love by<p> Zain Ali</p>
+          Made with passion by<p> Zain Ali</p>
         </div>
       </div>
     </>
